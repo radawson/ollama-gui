4e9ef76ea62f29c6d6ca11c61dccf0fbd0b6d176
Rick Dawson <dawsora@gmail.com>
2024-02-27 13:27:15 -0500

    messing with the avatar and base url

--- src/services/appConfig.ts
+++ src/services/appConfig.ts
@@ -2,10 +2,13 @@ import { useLocalStorage } from '@vueuse/core'
 import gravatarUrl from 'gravatar-url'
 import { computed } from 'vue'
 
+const BASE_URL = 'http://10.10.10.8:11434/api'
+
 export const gravatarEmail = useLocalStorage('gravatarEmail', 'helge.sverre@gmail.com')
-export const avatarUrl = computed(() => gravatarUrl(gravatarEmail.value, { size: 200 }))
+//export const avatarUrl = computed(() => gravatarUrl(gravatarEmail.value, { size: 200 }))
 export const debugMode = useLocalStorage('debug', false)
-export const baseUrl = useLocalStorage('baseUrl', 'http://localhost:11434/api')
+export const baseUrl = useLocalStorage('baseUrl', BASE_URL)
+export const avatarUrl = `/assets/avatar.png`
 export const isDarkMode = useLocalStorage('darkMode', true)
 export const isSettingsOpen = useLocalStorage('settingsPanelOpen', true)
 export const toggleSettingsPanel = () => (isSettingsOpen.value = !isSettingsOpen.value)
