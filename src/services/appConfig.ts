cf6dd7701c99d8d2ee0a7698f05a211f2ec0ddaa
Rick Dawson <dawsora@gmail.com>
2024-02-27 13:29:26 -0500

    avatar things

--- src/services/appConfig.ts
+++ src/services/appConfig.ts
@@ -8,7 +8,7 @@ export const gravatarEmail = useLocalStorage('gravatarEmail', 'helge.sverre@gmai
 //export const avatarUrl = computed(() => gravatarUrl(gravatarEmail.value, { size: 200 }))
 export const debugMode = useLocalStorage('debug', false)
 export const baseUrl = useLocalStorage('baseUrl', BASE_URL)
-export const avatarUrl = `/assets/avatar.png`
+export const avatarUrl = 'assets/avatar.png'
 export const isDarkMode = useLocalStorage('darkMode', true)
 export const isSettingsOpen = useLocalStorage('settingsPanelOpen', true)
 export const toggleSettingsPanel = () => (isSettingsOpen.value = !isSettingsOpen.value)
