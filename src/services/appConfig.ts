ea9380697553e3222cc964dbc293362864a5e4ff
Rick Dawson <dawsora@gmail.com>
2024-02-28 11:52:26 -0500

    change gravatar to default

--- src/services/appConfig.ts
+++ src/services/appConfig.ts
@@ -4,11 +4,10 @@ import { computed } from 'vue'
 
 const BASE_URL = 'http://10.10.10.8:11434/api'
 
-export const gravatarEmail = useLocalStorage('gravatarEmail', 'helge.sverre@gmail.com')
-//export const avatarUrl = computed(() => gravatarUrl(gravatarEmail.value, { size: 200 }))
+export const gravatarEmail = useLocalStorage('gravatarEmail', '')
+export const avatarUrl = computed(() => gravatarUrl(gravatarEmail.value, { size: 200, default: 'mm' }))
 export const debugMode = useLocalStorage('debug', false)
 export const baseUrl = useLocalStorage('baseUrl', BASE_URL)
-export const avatarUrl = '/src/assets/avatar.png'
 export const isDarkMode = useLocalStorage('darkMode', true)
 export const isSettingsOpen = useLocalStorage('settingsPanelOpen', true)
 export const toggleSettingsPanel = () => (isSettingsOpen.value = !isSettingsOpen.value)
